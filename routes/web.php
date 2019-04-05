c71b0a4f2c904ed0a6209920eebacbdf2f5899a6
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2019-04-05 10:37:07 +0900

    [update] route

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,8 @@ Route::get('/{any}', function () {
     return view('app');
 })->where('any', '.*');
 
+Route::get('/public/{any}', function () {
+    return view('app');
+})->where('any', '.*');
+
 
