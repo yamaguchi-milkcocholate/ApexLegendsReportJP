e08d004d23b91e72a360f2ae72de70416a54648b
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2019-04-05 10:14:03 +0900

    [update] https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
