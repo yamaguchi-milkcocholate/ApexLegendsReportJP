eb35308f00ecdca1bb31da80bc9f24ac8a589ea9
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2019-04-06 11:22:13 +0900

    [update]

--- app/Services/ChromeDriverService.php
+++ app/Services/ChromeDriverService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Chrome\ChromeDriver;
 
 /**
  * Class ChromeDriverService
@@ -24,6 +25,7 @@ class ChromeDriverService
 
     public function __construct()
     {
+        /*
         $this->seleniumServerUrl = 'http://0.0.0.0:4444/wd/hub';
         $this->options = (new ChromeOptions)->addArguments([
             '--disable-gpu',
@@ -32,6 +34,12 @@ class ChromeDriverService
             '--no-sandbox',
             '--disable-setuid-sandbox'
         ]);
+        */
+        putenv('webdriver.chrome.driver=/app/.chromedriver/bin/chromedriver');
+        $options = new ChromeOptions();
+        $chromeBinary = getenv("GOOGLE_CHROME_SHIM");
+        $options->setBinary($chromeBinary);
+        $this->options = $options;
     }
 
     /**
@@ -39,9 +47,14 @@ class ChromeDriverService
      */
     public function Driver()
     {
+        /*
         $capabilities = DesiredCapabilities::chrome();
         $capabilities->setCapability(ChromeOptions::CAPABILITY, $this->options);
         $driver = RemoteWebDriver::create($this->seleniumServerUrl, $capabilities);
         return $driver;
+        */
+        $capabilities = DesiredCapabilities::chrome();
+        $capabilities->setCapability(ChromeOptions::CAPABILITY, $this->options);
+        return ChromeDriver::start($capabilities);
     }
 }
