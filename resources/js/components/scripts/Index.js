1d05f7da059b4792c51d73d72492f60868907dc5
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2019-04-07 20:32:48 +0900

    [update]

--- resources/js/components/scripts/Index.js
+++ resources/js/components/scripts/Index.js
@@ -103,7 +103,7 @@ export default {
                 console.log('報告中に新しい報告はできません!!');
                 return;
             }
-            let url =  + 'http://apex-legends-report-jp.herokuapp.com/public/api/v1/report';
+            let url = 'http://apex-legends-report-jp.herokuapp.com/public/api/v1/report';
             let params = {
                 id: this.your_id,
                 email: this.your_email,
